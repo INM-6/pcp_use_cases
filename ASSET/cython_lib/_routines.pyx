4e652298f0a1c987f7b423410a6dbccc5d37b3d4
klijn1 <nonoice@gmail.com>
2017-04-19 16:34:50 +0200

    Additional cleanup of the external lib

--- ASSET/cython_lib/_routines.pyx
+++ ASSET/cython_lib/_routines.pyx
@@ -8,12 +8,6 @@ import numpy as np
 cimport numpy as np
 import cython
 
-# Floating point types to use with NumPy
-#
-# IEEE 754 says that float is 32 bits and double is 64 bits
-#
-ctypedef double dtype_float_t
-
 cdef extern from "_routines.hpp":
     void expopt_array(float* input, int entries, float logK)
 
@@ -28,6 +22,6 @@ def exp_opt_array(np.ndarray[float, ndim=2] input, logK):
     """
     input = np.ascontiguousarray(input)
     expopt_array(&input[0,0], 
-                    input.shape[0] * input.shape[1],
-					logK)
+                 input.shape[0] * input.shape[1],
+                 logK)
 
